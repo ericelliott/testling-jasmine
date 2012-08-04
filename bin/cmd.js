7da1a564cbbd2ec04fda5173cd5450078779fe4a
James Halliday <mail@substack.net>
2012-08-03 17:02:20 -0700

    package.json and whatevers

---
+++ bin/cmd.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+
+var request = require('request');
+var qs = require('querystring');
+var fs = require('fs');
+var path = require('path');
+var deepMerge = require('deepmerge');
+var read = require('read');
+
+var argv = require('optimist').argv;
+var files = [ __dirname + '/../browser.js' ].concat(argv._)
+    .reduce(function (acc, file) {
+        var ext = path.extname(file).replace(/^\./, '');
+        if (!acc[ext]) acc[ext] = [];
+        acc[ext].push(file);
+        return acc;
+    }, {})
+;
+if (files.js.length === 1) {
+    return console.error(
+        'Usage: testling-jasmine OPTIONS [json files, test files]'
+    );
+}
+
+var body = [
+    'exports=undefined',
+    '(function () {',
+    files.js.map(function (file) {
+        return fs.readFileSync(file, 'utf8');
+    }).join(';\n'),
+    '}).call(window)',
+].join(';\n');
+
+var config = (files.json || []).reduce(function (acc, file) {
+    return deepMerge(acc, JSON.parse(fs.readFileSync(file)));
+}, argv);
+
+var params = {
+    script : config.script || config.scripts,
+    browsers : Array.isArray(config.browsers)
+        ? config.browsers.join(',')
+        : config.browsers
+    ,
+};
+if (!config.browsers) delete params.browsers;
+if (!config.script) delete params.script;
+
+if (config.user) {
+    withUser(config.user);
+}
+else {
+    read({ prompt : 'username: ' }, function (err, res) {
+        if (err) console.error(err)
+        else withUser(res)
+    });
+}
+
+function withUser (user) {
+    if (!config.pass) {
+        read({ prompt : 'password: ', silent : true }, function (err, res) {
+            if (err) console.error(err)
+            else withAuth(user, res)
+        });
+    }
+    else withAuth(user, config.pass)
+}
+
+function withAuth (user, pass) {
+    var uri = 'http://'
+        + encodeURIComponent(user) + ':'
+        + encodeURIComponent(pass) + '@'
+        + 'testling.com/'
+        + '?' + qs.stringify(params)
+        + (config.instrument ? '' : '&noinstrument')
+    ;
+    
+    if (argv.print) {
+        return console.log(uri);
+    }
+    
+    var r = request.put({ uri : uri, body : body });
+    r.pipe(process.stdout, { end : false });
+}
--- readme.markdown
+++ readme.markdown
@@ -5,12 +5,30 @@ Run jasmine tests in all the browsers with testling.
 # example
 
 ```
- $ eval $(./build.sh substack@gmail.com)
-Enter host password for user 'substack@gmail.com':
-chrome/14.0:
+$ testling-jasmine example/test.js example/test.json
+username: substack@gmail.com
+password: 
+iexplore/9.0:
   mixinTest:should add mixins ....................... 1/1
   mvc plugin:should exist ........................... 1/1
   overrideTest:should let module override mixins .... 1/1
 
-total ............................................... 3/3
+firefox/12.0:
+  mixinTest:should add mixins ....................... 1/1
+  mvc plugin:should exist ........................... 1/1
+  overrideTest:should let module override mixins .... 1/1
+
+total ............................................... 6/6
 ```
+
+# install
+
+With [npm](http://npmjs.org) do:
+
+```
+npm install -g testling-jasmine
+```
+
+# license
+
+MIT
